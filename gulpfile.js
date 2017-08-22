596f757337e4a3e601b31a8b2ddbdd56ac94e68f
rafaelrumpel <rafaelgoerl@gmail.com>
2017-08-22 15:41:53 -0300

    - Removing

--- gulpfile.js
+++ gulpfile.js
@@ -9,34 +9,34 @@ var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('build-css', function () {
-  return gulp.src('./styles/**/*.scss')
+  return gulp.src('./src/styles/**/*.scss')
     .pipe(sourcemaps.init())  // Process the original sources
       .pipe(sass())
     .pipe(sourcemaps.write()) // Add the map to modified source.
-    .pipe(gulp.dest('./build/css'));
+    .pipe(gulp.dest('./'));
 });
 
 gulp.task('jshint', function () {
-  return gulp.src('./scripts/**/*.js')
+  return gulp.src('./src/scripts/**/*.js')
     .pipe(jshint())
     .pipe(jshint.reporter('jshint-stylish'));
 });
 
 gulp.task('build-js', function () {
-  return gulp.src('./scripts/**/*.js')
+  return gulp.src('./src/scripts/**/*.js')
     .pipe(sourcemaps.init())
       .pipe(concat('scribblelivefeed.js'))
       //only uglify if gulp is ran with '--type production'
       .pipe(gutil.env.type === 'production' ? uglify() : gutil.noop())
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./build/js'));
+    .pipe(gulp.dest('./'));
 });
 
 // configure which files to watch and what tasks to use on file changes
 gulp.task('watch', function () {
-  gulp.watch('./scripts/**/*.js', ['jshint']);
-  gulp.watch('./scripts/**/*.js', ['build-js']);
-  gulp.watch('./styles/**/*.scss', ['build-css']);
+  gulp.watch('./src/scripts/**/*.js', ['jshint']);
+  gulp.watch('./src/scripts/**/*.js', ['build-js']);
+  gulp.watch('./src/styles/**/*.scss', ['build-css']);
 });
 
 // define the default task and add the watch task to it
